17592a41db960ddd4168a8723334d460004894e3
Rakesh Ranjan <rakranja1@publicisgroupe.net>
2018-08-17 17:03:47 +0530

    About Component added

--- portfolio/portfolio/src/app/app.module.ts
+++ portfolio/portfolio/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ImageCard2x1Component } from './image-card2x1/image-card2x1.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageCard2x1Component
+    ImageCard2x1Component,
+    AboutComponent
   ],
   imports: [
     BrowserModule
